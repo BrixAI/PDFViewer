80c17c063fef10cf2a074efb6758372ab9520c4a
Srikar Parsi <srikar.parsi@expensify.com>
2025-03-12 01:15:58 -0400

    suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import PDFViewer from "./pdfViewer";
 
 export default function PDFViewerPage() {
   const searchParams = useSearchParams();
   const document1 = searchParams.get("doc1") || "";
   const document2 = searchParams.get("doc2") || "";
-  console.log("DOCUMENTS");
-  console.log(document1);
-  console.log(document2);
 
-  return <PDFViewer document1={document1} document2={document2} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PDFViewer document1={document1} document2={document2} />
+    </Suspense>
+  );
 }
